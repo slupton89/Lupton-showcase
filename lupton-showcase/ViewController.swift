7978138c3df379f8a51065a373bfb4e4a07d2fb5
Shane Lupton <slupton89@icloud.com>
2016-02-25 23:25:51 -0700

    Initial Commit

---
+++ lupton-showcase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lupton-showcase
+//
+//  Created by Shane Lupton on 2/25/16.
+//  Copyright © 2016 Shane Lupton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
